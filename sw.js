45b4b69b0ae205cab3805bee81396887ebb27b23
zhengchendell508-bit <zhengchendell508@gmail.com>
2026-07-11 21:01:07 -0500

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='english-test-admin-demo-v2';
-const ASSETS=['./','./index.html','./style.css','./data.js','./script.js','./admin.html','./admin.css','./admin.js','./manifest.webmanifest'];
+const CACHE='english-test-v2';
+const ASSETS=['./','./index.html','./student.html','./admin.html','./admin.css','./admin.js','./style.css','./data.js','./script.js','./manifest.webmanifest'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
 self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
