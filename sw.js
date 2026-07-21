df85e927d8399197788de3ce48aba5724f17b6b6
zhengchendell508-bit <zhengchendell508@gmail.com>
2026-07-20 19:47:16 -0500

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,46 @@
-const CACHE='english-test-v8-timer-visible';
-const ASSETS=['./','./index.html','./student.html','./admin.html','./admin.css','./admin.js','./style.css','./data.js','./script.js','./translations.js','./manifest.webmanifest'];
-self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
-self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
-self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
+const CACHE = "english-test-landscape-report-v9";
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./parent.html",
+  "./children.html",
+  "./student.html",
+  "./admin.html",
+  "./style.css",
+  "./account.js",
+  "./device-mode.js",
+  "./data.js",
+  "./student.js",
+  "./admin.js"
+];
+
+self.addEventListener("install", event => {
+  self.skipWaiting();
+  event.waitUntil(
+    caches.open(CACHE).then(cache => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys()
+      .then(keys => Promise.all(
+        keys.filter(key => key !== CACHE).map(key => caches.delete(key))
+      ))
+      .then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener("fetch", event => {
+  if (event.request.method !== "GET") return;
+
+  event.respondWith(
+    fetch(event.request)
+      .then(response => {
+        const copy = response.clone();
+        caches.open(CACHE).then(cache => cache.put(event.request, copy));
+        return response;
+      })
+      .catch(() => caches.match(event.request))
+  );
+});
